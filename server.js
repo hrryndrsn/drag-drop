1962d2b52775d9cf6fce81d11a59ab5e72970aa3
harryanderson <harryanderson9@gmail.com>
2017-05-27 13:17:51 +1000

    dynamic post URL working on some sub dirs

--- server.js
+++ server.js
@@ -11,15 +11,6 @@ var app = express();
 
 app.set('view engine', 'hbs');
 
-hbs.registerHelper('getDropzoneJS', () => {
-    var path = '/js/dropzone.js'
-    return path
-});
-
-hbs.registerHelper('getDropzoneCSS', () => {
-    var path = '/css/dropzone.css'
-    return path
-});
 
 // setup static file server for public dir
 app.use(express.static(__dirname + '/public'));
@@ -27,31 +18,16 @@ app.use(express.static(__dirname + '/public'));
 //logger middleware
 app.use((req, res, next) => {
     var now = new Date().toString();
-    console.log(`${now}: ${req.method} ${req.url}`);
+    console.log(` TIME: ${now} ~ METHOD | ${req.method}, URL | ${req.url}`);
     next();    
 });
 
-//route for /uploads
-app.use('/uploads', serveIndex(
-    
-    'public/uploads', 
-    {'icons': true,
-    'template': 'views/template.hbs',
-    'display mode': 'details',
-    'stylesheet': 'css/style.css',
-    'view': 'details'
-
-    }
-    
-    ));
-
-
-//route for uploading files ONLY SINGLE FILES WORKS 
-app.post('/', upload.any(), function (req, res, next) {
+// /uploads/ route
+app.post('/uploads/', upload.any(), function (req, res, next) {
     
     //set the file
-    //console.log(req.files, 'files');
-    //console.log(req.files[0].originalname, 'file name')
+    console.log(req.files, 'files');
+    console.log(req.files[0].originalname, 'file name')
     var fileName = req.files[0].originalname;
     var fileSize = req.files[0].size;
     var tempPath = req.files[0].path;
@@ -71,6 +47,75 @@ app.post('/', upload.any(), function (req, res, next) {
     return res.status(200).send('success');
     
 });
+
+// /uploads/3D/  route
+app.post('/uploads/3D/', upload.any(), function (req, res, next) {
+    
+    //set the file
+    console.log(req.files, 'files');
+    console.log(req.files[0].originalname, 'file name')
+    var fileName = req.files[0].originalname;
+    var fileSize = req.files[0].size;
+    var tempPath = req.files[0].path;
+    var uploadPath = __dirname + '/public/uploads/3D/' + fileName;
+
+    //saving the file
+    console.log(`SERVER LOG ~ file: ${fileName} size: ${fileSize} `);
+    console.log(`SERVER LOG ~ uploading file to : ${uploadPath}`);
+    
+    //Move the file
+    fs.rename(tempPath, uploadPath, (err) => {
+        if (err) {
+            console.log('POST ERROR || ' + err);
+        }
+    });
+    
+    return res.status(200).send('success');
+    
+});
+
+// serves-index file stucture SHOULD BE AT THE BOTTOM
+app.use('/uploads', serveIndex(
+    
+    'public/uploads', 
+    {'icons': true,
+    'template': 'views/template.hbs',
+    'display mode': 'details',
+    'stylesheet': 'css/style.css',
+    'view': 'details'
+
+    }
+    
+    ));
+
+
+//route for uploading files ONLY SINGLE FILES WORKS 
+app.post('/', upload.any(), function (req, res, next) {
+    
+    //set the file
+    console.log(req.files, 'files');
+    console.log(req.files[0].originalname, 'file name')
+    var fileName = req.files[0].originalname;
+    var fileSize = req.files[0].size;
+    var tempPath = req.files[0].path;
+    var uploadPath = __dirname + '/public/uploads/' + fileName;
+
+    //saving the file
+    console.log(`SERVER LOG ~ file: ${fileName} size: ${fileSize} `);
+    console.log(`SERVER LOG ~ uploading file to : ${uploadPath}`);
+    
+    //Move the file
+    fs.rename(tempPath, uploadPath, (err) => {
+        if (err) {
+            console.log('POST ERROR || ' + err);
+        }
+    });
+    
+    return res.status(200).send('success');
+    
+});
+
+
 
 
 app.listen(3000, () => {
